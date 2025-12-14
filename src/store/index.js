513480cb091355d8f72938e362ff3235a03fc327
ytming <3163547035@qq.com>
2025-12-15 00:51:48 +0800

    Add files via upload

--- src/store/index.js
+++ src/store/index.js
@@ -6,25 +6,25 @@ export const mainStore = defineStore("main", {
       imgLoadStatus: false, // 壁纸加载状态
       innerWidth: null, // 当前窗口宽度
       coverType: "0", // 壁纸种类
-      siteStartShow: false, // 建站日期显示
+      siteStartShow: true, // 建站日期显示
       musicClick: false, // 音乐链接是否跳转
       musicIsOk: false, // 音乐是否加载完成
-      musicVolume: 0, // 音乐音量;
+      musicVolume: 0.1, // 音乐音量;
       musicOpenState: false, // 音乐面板开启状态
       backgroundShow: false, // 壁纸展示状态
       boxOpenState: false, // 盒子开启状态
       mobileOpenState: false, // 移动端开启状态
-      mobileFuncState: false, // 移动端功能区开启状态
+      mobileFuncState: true, // 移动端功能区开启状态
       setOpenState: false, // 设置页面开启状态
       playerState: false, // 当前播放状态
       playerTitle: null, // 当前播放歌曲名
       playerArtist: null, // 当前播放歌手名
-      playerLrc: "歌词加载中", // 当前播放歌词
+      playerLrc: "歌词马上到达~", // 当前播放歌词
       playerLrcShow: true, // 是否显示底栏歌词
       footerBlur: true, // 底栏模糊
       playerAutoplay: false, // 是否自动播放
       playerLoop: "all", // 循环播放 "all", "one", "none"
-      playerOrder: "list", // 循环顺序 "list", "random"
+      playerOrder: "list", // 循环顺序 "list", "random"      
     };
   },
   getters: {
@@ -69,7 +69,7 @@ export const mainStore = defineStore("main", {
     setPlayerData(title, artist) {
       this.playerTitle = title;
       this.playerArtist = artist;
-    },
+    }, 
     // 更改壁纸加载状态
     setImgLoadStatus(value) {
       this.imgLoadStatus = value;
