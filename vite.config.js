beff5142fa0eb2ffeb5f85dd744b2a12ff8238c6
ytming <3163547035@qq.com>
2025-12-15 18:20:13 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,11 @@
 import { defineConfig, loadEnv } from "vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import { resolve } from "path";
-import { VitePWA } from "vite-plugin-pwa";
+//import { VitePWA } from "vite-plugin-pwa";// 1.不使用PWA
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import viteCompression from "vite-plugin-compression";
-
-// https://vitejs.dev/config/
 export default ({ mode }) =>
   defineConfig({
     plugins: [
@@ -20,6 +18,8 @@ export default ({ mode }) =>
       Components({
         resolvers: [ElementPlusResolver()],
       }),
+      // 2. 注释掉整个 VitePWA 配置块
+      /*
       VitePWA({
         registerType: "autoUpdate",
         workbox: {
@@ -89,6 +89,7 @@ export default ({ mode }) =>
           ],
         },
       }),
+      */
       viteCompression(),
     ],
     server: {
